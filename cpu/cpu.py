98fab8ae1875bae0e8c2af1a64172af3447404b1
Hellixaro <lukashenriksson08@gmail.com>
2025-10-08 10:22:57 +0200

    Small fix

--- cpu/cpu.py
+++ cpu/cpu.py
@@ -1,5 +1,5 @@
 regs = {
-    **{f"r{i}": "0" * 16 for i in range(15)},
+    **{f"r{i}": "0" * 16 for i in range(16)},
     "funcret": "0" * 16,
     "cmpreg": "000",
     "pc": "0" * 16,
---
+++ tests/debg.asm
@@ -0,0 +1,6 @@
+ldi 65535 r1
+add dbg r1
+ldi 10 r2
+ldi 1 r3
+
+mov r1 dbg
