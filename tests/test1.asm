9338cb5d276335ea1e804ce60d37a453a9cd171b
Hellixaro <lukashenriksson08@gmail.com>
2026-01-28 15:35:57 +0100

    tests stuff

--- tests/test1.asm
+++ tests/test1.asm
@@ -1,6 +1,6 @@
-labl hej
+labl hej    ; creates a lable
 ldi 12 r0   ; Load 12 into r0
 ldi 6 r1    ; Load 6 into r1
-add r0 r1   ; Add the contents of r0 and r1 and puts it in r0
+add r0 r1   ; r0 + r1 into r0, operand 1 + operand 2 into operand 1
 mov r0 dbg  ; Display r0 as the output
-jmp hej
+jmp hej     ; Jumps back to the lable
